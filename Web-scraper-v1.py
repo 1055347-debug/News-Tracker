691205e75d63a7a0c676e4b92e4d20bd7f34fb52
AIDANOOOOO <1055347@etusd.org>
2026-03-28 12:43:24 -0700

    Update Web-scraper-v1.py

--- Web-scraper-v1.py
+++ Web-scraper-v1.py
@@ -1,5 +1,5 @@
 from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key="bfb8aed228c54fd99333d7dc74a436ea")
+newsapi = NewsApiClient(api_key="ICANTSHARE")
 what_you_want = input("What topic do you want??: ")
 what_source = input("What source do you want??: ")
 biggest_headlines = newsapi.get_everything(q = f'{what_you_want}', sources = f"{what_source}")
