70a5468f8acd25d1ac808eee67385ff38fcb7b0c
AIDANOOOOO <1055347@etusd.org>
2026-03-28 12:40:34 -0700

    Update Web-scraper-v1.py

--- Web-scraper-v1.py
+++ Web-scraper-v1.py
@@ -4,14 +4,7 @@ what_you_want = input("What topic do you want??: ")
 what_source = input("What source do you want??: ")
 biggest_headlines = newsapi.get_everything(q = f'{what_you_want}', sources = f"{what_source}")
 articles = biggest_headlines["articles"]
-count = 0
-OIlprice = 0
 for x in articles:
     print(f"According to {x["source"]["name"]} {x["description"]}")
-    count += 1
-    if what_you_want == "Oil":
-        for x in articles:
-            if "risk" or "Risk" in x["description"]:
-                OIlprice += 1
-print(count)
-print(f"The Oil price risk is currently {OIlprice/count}")
+
+
